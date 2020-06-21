e2fc82bd3131c295f870d2503f774e58e5607966
hauptdigital <marc@haupt.digital>
2020-06-21 13:04:38 +0200

    Add husky hook to use ESlint on files before commit

--- app/app.ts
+++ app/app.ts
@@ -1,13 +1,13 @@
 // lib/app.ts
-import express = require("express");
+import express = require('express');
 
 // Create a new express application instance
 const app: express.Application = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World!");
+app.get('/', function (req, res) {
+    es.send('Hello World!');
 });
 
 app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
+    console.log('Example app listening on port 3000!');
 });
