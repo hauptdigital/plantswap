f52a2b13d59e033acad44acfd7f4d536e833fcc4
hauptdigital <marc@haupt.digital>
2020-07-10 17:53:41 +0200

    Add helper function to create random hex string with custom length

--- src/lib/utils/utils.js
+++ src/lib/utils/utils.js
@@ -1,6 +1,15 @@
+const crypto = require('crypto');
+
 function isValidEmail(email) {
     const emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,10})?$/;
     return emailRegex.test(email);
 }
 
+function generateRandomUrlFriendlyString(size) {
+    const randomString = crypto.randomBytes(size).toString('hex').slice(0, size);
+    console.log(randomString);
+    return randomString;
+}
+
 module.exports.isValidEmail = isValidEmail;
+module.exports.generateRandomUrlFriendlyString = generateRandomUrlFriendlyString;
